80148cbcf18d02807dbe00187678cf3de725f084
Ramakrishna <medidha1100@gmail.com>
2017-06-29 23:38:01 +0530

    added NODE JS
    added NODE JS

---
+++ other Libraries/Node JS/NodeJSTutorial.js
@@ -0,0 +1,23 @@
+//NodeJS
+
+console.log("Welcome to Node JS");
+
+//2)server creation
+
+var http=require('http');
+var server=http.createServer(function(req,resp){
+	resp.writeHead(200,{"Content-Type":"text/plain"});
+	resp.end("Welcome to NodeJS");
+});
+server.listen(1234,function(){
+	console.log("Startd");
+});
+//3)to install
+npm ls (to find list of installed modules)
+npm ls -g (to find list of installed modules globally)
+npm view modulname(angular)
+npm install moduleName(angular,exptress etc)
+//4)to get all outdated modules
+npm outdated
+//5)toupdate all outdated modules
+npm update --save 
