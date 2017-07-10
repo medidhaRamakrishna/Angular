d9b9e2f4314876c70f744ee80e8558bd0cb80938
Ramakrishna <medidha1100@gmail.com>
2017-07-10 23:42:05 +0530

    commited angular2 files
    commited angular2 files

--- other Libraries/angular2/HeroApp/quickstart-master/src/app/app.component.ts
+++ other Libraries/angular2/HeroApp/quickstart-master/src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
- 
-export class Hero {
-  id: number;
-  name: string;
-}
- 
+ import { Hero } from './hero';
+
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
@@ -21,24 +17,7 @@ const HEROES: Hero[] = [
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]==="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-      
-    </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+    Hello
   `,
   styles: [`
     .selected {
@@ -98,4 +77,4 @@ export class AppComponent {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
+} 
---
+++ other Libraries/angular2/HeroApp/quickstart-master/src/app/hero-detail.compoent.js
@@ -0,0 +1,9 @@
+"use strict";
+;
+expect;
+var HeroDetailComponent = (function () {
+    function HeroDetailComponent() {
+    }
+    return HeroDetailComponent;
+}());
+//# sourceMappingURL=hero-detail.compoent.js.map
