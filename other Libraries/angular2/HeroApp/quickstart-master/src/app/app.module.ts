1fc3fe5dd6975c069c37bdb72124ddcf260abf72
Ramakrishna <medidha1100@gmail.com>
2017-07-10 23:19:59 +0530

    added angular js2
    added angular js2

---
+++ other Libraries/angular2/HeroApp/quickstart-master/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';//ngModel lives here
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule,
+  FormsModule// <-- import the FormsModule before binding with [(ngModel)]
+   ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
