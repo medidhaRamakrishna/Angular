a6534dec9f3f70c94fd210676d38f005da4bbaca
Ramakrishna <medidha1100@gmail.com>
2017-07-13 01:17:46 +0530

    contact app in angular js
    contact app in angular js

--- other Libraries/CONTACT APP IN ANGULAR/app.js
+++ other Libraries/CONTACT APP IN ANGULAR/app.js
@@ -1,12 +1,51 @@
 var app=angular.module('myApp',[]);
 app.controller('contactController',contactsFunction);
 
-function contactsFunction($scope,){
+function contactsFunction($scope){
 $scope.enabledEdit =[];
 //  $scope.First_Name='jhsdgvjhsdghvj';
+$scope.AddContact=function(){
+console.log($scope.firstName);
+
+ if($scope.firstName && $scope.Middle_Name && $scope.lastName && $scope.contactNumber && $scope.Contact_Email){
+   console.log("scope.firstName");
+   //checking for uniqueness
+  var retObj=$scope.contactList.filter(function(obj){
+     return (obj.contactNumber==$scope.contactNumber ||obj.contactEmail==$scope.Contact_Email)
+   });
+   if(retObj.length>0){
+     alert("This contact Number/Email is already exist"); 
+     return;
+   }
+ //pushing into array
+
+   $scope.contactList.push({"firstName":$scope.firstName,"lastName":$scope.lastName,"contactNumber":$scope.contactNumber,"contactEmail":$scope.Contact_Email});
+   $scope.firstName='';
+   $scope.lastName='';
+   $scope.Middle_Name="";   
+   $scope.contactNumber='';
+   $scope.Contact_Email='';
+
+
+ }else{
+   
+
+   alert("Please enter valid details before adding a contact");
+ }
+
+}
 $scope.editContact=function(index){
-    console.log("edit index"+index);
-	$scope.enabledEdit[index] = true;    
+   // console.log("edit index"+index+"TRUE/FALSE"+$scope.enabledEdit[index]);
+	if($scope.enabledEdit[index] == undefined){
+    $scope.enabledEdit[index]=true;
+  }else{
+    $scope.enabledEdit[index]=!$scope.enabledEdit[index];
+  }
+}
+
+$scope.contactlength=function(){
+  //alert($scope.contactNumber.length);
+  alert();
 }
   $scope.contactList = [
 {
