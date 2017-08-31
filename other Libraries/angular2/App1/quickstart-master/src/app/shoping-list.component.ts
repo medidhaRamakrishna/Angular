8e133d130c9fffb878ebdb469abe87792a69dcaf
Ramakrishna <medidha1100@gmail.com>
2017-09-01 00:43:53 +0530

    added App one

--- other Libraries/angular2/App1/quickstart-master/src/app/shoping-list.component.ts
+++ other Libraries/angular2/App1/quickstart-master/src/app/shoping-list.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
 @Component({
 selector:'shopping-list',
-template:`
-<ul class="container">
-<li *ngFor="let item of Items_List" (click)="editItem(item)">{{item.name}}&nbsp;&nbsp;&nbsp;<button (click)="deleteItem(item)"><span style="color:red">X</span></button></li>
-</ul>
-<input type="text" [(ngModel)]="selectedItem.name" #itemRef>
+template:`<div>
+<div class="form-group contacts1"><input type="text"  maxlength="20" [(ngModel)]="selectedItem.name" #itemRef>
 <button (click)="addItem(itemRef)">Add Item</button>
+<table  border="1" class="container" class="contacts">
+<tr *ngFor="let item of Items_List  " (click)="editItem(item)" >
+<td>{{item.name}}</td>
+<td><button (click)="deleteItem(item)" class="glyphicon glyphicon-trash">Delete</button></td>
+</tr>
+</table>
+</div>
 `
 })
 
@@ -20,10 +24,10 @@ editItem(item:any){
 this.selectedItem=item;
 }
 addItem(itemRef:any){
-    if(itemRef. value!='')
+    if(itemRef.value!='')
 this.Items_List.push({name:itemRef.value})
 }
-deleteItem(item:any){
+deleteItem(item:any){   
 this.Items_List.splice(this.Items_List.indexOf(item),1);
 }
 }
